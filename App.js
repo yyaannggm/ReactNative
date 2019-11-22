492d1c2780a9b92da73dd6b8a5892f89d48ce199
yyaannggm <15001760091@163.com>
2019-11-22 23:34:25 +0800

    test

--- App.js
+++ App.js
@@ -1,54 +1,90 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
-    StyleSheet,
     View,
-    TextInput,
-    Button,
     Text,
-    Dimensions
-} from 'react-native';
+    Button,
+    Keyboard,
+    Platform,
+    TextInput,
+    StyleSheet,
+    ScrollView,
+} from 'react-native'
 
-
-const screenWidth = Dimensions.get('window').width;
-const screenHeight = Dimensions.get('window').height;
-
-
-export default class countDown extends Component {
+export default class Home extends Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
-
+            keyBoardHeight: 0
         }
     }
+    componentWillMount() {
+        this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this._keyboardDidShow.bind(this));
+        this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide.bind(this));
+    }
+    componentWillUnmount() {
+        this.keyboardDidShowListener.remove();
+        this.keyboardDidHideListener.remove();
+    }
+    _keyboardDidShow(e) {
+        this.setState({
+            keyBoardHeight: e.endCoordinates.height
+        });
+    }
+    _keyboardDidHide() {
+        this.setState({
+            keyBoardHeight: 0
+        });
+    }
+    loseFocus() {
+        this.refs.input.blur()
+    }
     render() {
-        let array = ["a", "b", "c", "d", "e", "f", "g"]
-        let str = "12345667"
-        let noShow = array.length - 4
+        let { keyBoardHeight } = this.state
         return (
-            <View>
-                <Text>{str.length}</Text>
+            <View style={css.container}>
+                <ScrollView style={{ paddingBottom: 100 }}>
+                    <Text>height: {keyBoardHeight}</Text>
+                    <View style={{ backgroundColor: 'red', height: 500 }}></View>
+                    <Button style={css.txt} onPress={() => this.loseFocus()}></Button>
+                    <View style={{ backgroundColor: 'blue', height: 500 }}></View>
+                    <View style={{ backgroundColor: 'green', height: 500 }}></View>
+                </ScrollView>
+                <View style={[css.box, Platform.OS == 'ios' && { bottom: keyBoardHeight }]}>
+                    <TextInput
+                        ref="input"
+                        style={css.input}
+                        underlineColorAndroid="transparent" />
+                </View>
             </View>
         )
     }
-
-
-
-
-
-
-
-
 }
 
-styles = StyleSheet.create({
-
-    text: {
-        marginTop: 100,
-        marginLeft: 40,
-        width: 200,
-        fontSize: 30
+const css = StyleSheet.create({
+    container: {
+        flex: 1
     },
-    last: {
-        fontSize: 40
+    input: {
+        height: 60,
+        width: '100%',
+        fontSize: 20,
+        color: '#333333',
+        backgroundColor: '#eee',
+        borderRadius: 60,
+        paddingHorizontal: 20,
+        paddingVertical: 0
+    },
+    box: {
+        backgroundColor: '#fff',
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    txt: {
+        color: 'red'
     }
-});
+})
