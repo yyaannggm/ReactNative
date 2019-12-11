4b3f209ded56295c860e470b49af3daad54cd86e
yyaannggm <15001760091@163.com>
2019-12-11 23:59:08 +0800

    sms

--- LearnRN/App.js
+++ LearnRN/App.js
@@ -1,47 +1,89 @@
-import React from 'react';
-import {StyleSheet, Dimensions, View} from 'react-native';
+import React, {PureComponent} from 'react';
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
+import lodash from 'lodash';
+import PropTypes from 'prop-types';
+import styles from './VerifyCode.style';
 
-import Pdf from 'react-native-pdf';
+const propTypes = {
+  onChangeText: PropTypes.func.isRequired, // 验证码实时变化值
+  verifyCodeLength: PropTypes.number.isRequired, // 验证码数
+};
 
-export default class PDFExample extends React.Component {
-  render() {
-    const source = {
-      uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
-      cache: true,
+const defaultProps = {
+  onChangeText: () => {},
+  verifyCodeLength: 6, // 默认6位
+};
+
+// 验证码组件
+class VerifyCode extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      verifyCode: '', // 验证码
     };
+    this.onTouchInput = this.onTouchInput.bind(this);
+  }
+
+  onTouchInput() {
+    const isFocused = this.textInput.isFocused();
+    if (!isFocused) {
+      this.textInput.focus();
+    }
+  }
+
+  renderVerifyCode(value) {
+    const {verifyCodeLength} = this.props;
+    const paddedValue = lodash.padEnd(value, verifyCodeLength, ' ');
+    const valueArray = paddedValue.split('');
     return (
-      <View style={styles.container}>
-        <Pdf
-          source={source}
-          onLoadComplete={(numberOfPages, filePath) => {
-            console.log(`number of pages: ${numberOfPages}`);
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={this.onTouchInput}
+        style={styles.verifyTextContainer}>
+        {valueArray.map((digit, index) => (
+          <View
+            key={index}
+            style={
+              digit === ' ' ? styles.textInputItem : styles.textInputItemIn
+            }>
+            <Text style={styles.verifyText}>{digit}</Text>
+          </View>
+        ))}
+      </TouchableOpacity>
+    );
+  }
+
+  render() {
+    const {verifyCode} = this.state;
+    const {onChangeText, verifyCodeLength} = this.props;
+    return (
+      <View style={styles.verifyContainer}>
+        {this.renderVerifyCode(verifyCode)}
+        <TextInput
+          ref={ref => {
+            this.textInput = ref;
           }}
-          onPageChanged={(page, numberOfPages) => {
-            console.log(`current page: ${page}`);
+          underlineColorAndroid="transparent"
+          caretHidden
+          style={styles.textInput}
+          autoFocus={true}
+          keyboardType={'numeric'}
+          maxLength={verifyCodeLength}
+          onChangeText={text => {
+            const reg = /^[0-9]*$/;
+            if (reg.test(text)) {
+              this.setState({verifyCode: text});
+              onChangeText(text);
+            }
           }}
-          onError={error => {
-            console.log(error);
-          }}
-          onPressLink={uri => {
-            console.log(`Link presse: ${uri}`);
-          }}
-          style={styles.pdf}
+          value={verifyCode}
         />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    marginTop: 25,
-  },
-  pdf: {
-    flex: 1,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
+VerifyCode.propTypes = propTypes;
+VerifyCode.defaultProps = defaultProps;
+
+export default VerifyCode;
