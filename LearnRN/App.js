6e0e5dab302c6bbf9bf40893f68b73f861aefe51
yyaannggm <15001760091@163.com>
2019-12-22 21:44:02 +0800

    download

--- LearnRN/App.js
+++ LearnRN/App.js
@@ -1,52 +1,23 @@
-import React, {Component} from 'react';;
 import RNFS from 'react-native-fs';
 
-export default class VerifyCode extends Component {
-  downloadFile() {
-    const downloadDest = `${RNFS.MainBundlePath}/${(Math.random() * 1000) |
-      0}.mp3`;
-
-    const formUrl =
-      'http://wvoice.spriteapp.cn/voice/2015/0818/55d2248309b09.mp3'
-    const options = {
-      fromUrl: formUrl,
-      toFile: downloadDest,
-      background: true,
-      begin: res => {
-        console.log('begin', res);
-        console.log('contentLength:', res.contentLength / 1024 / 1024, 'M');
-      },
-      progress: res => {
-        let pro = res.bytesWritten / res.contentLength;
-
-        this.setState({
-          progressNum: pro,
-        });
-      }
-    };
-    try {
-      const ret = RNFS.downloadFile(options);
-      ret.promise
-        .then(res => {
-          console.log('success', res);
-
-          console.log('file://' + downloadDest);;
-
-
-            CameraRoll.saveToCameraRoll(downloadDest)
-            .then(() => {
-              Toast.showShortCenter('图片已保存到相册');;
-            })
-            .catch(() => {
-              Toast.showShortCenter('图片保存失败');;
-            });
-        })
-        .catch(err => {
-          console.log('err', err);
-        });
-    } catch (e) {
-      console.log(error);
-    }
-  }
-}
-
+const ExternalDirectoryPath = RNFS.ExternalDirectoryPath;
+export const downloadFile = (formUrl, targetName) => {
+  const toLoadPath = `${ExternalDirectoryPath}/${targetName}`;
+  RNFS.downloadFile({
+    fromUrl: formUrl,
+    toFile: toLoadPath,
+    progressDivider: 5,
+    begin: res => {
+      console.log('begin', res);
+    },
+    progress: res => {
+      console.log('progress', res);
+    },
+  })
+    .promise.then(res => {
+      console.log(res, '下载成功!!');
+    })
+    .catch(err => {
+      console.log(err, '下载失败!!');
+    });
+};
