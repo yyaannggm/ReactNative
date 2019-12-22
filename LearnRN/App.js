ac5006ec129c9d6d2d8b18fcea75a33761e219c6
yyaannggm <15001760091@163.com>
2019-12-22 21:40:39 +0800

    download

--- LearnRN/App.js
+++ LearnRN/App.js
@@ -1,25 +1,52 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';;
+import RNFS from 'react-native-fs';
 
 export default class VerifyCode extends Component {
+  downloadFile() {
+    const downloadDest = `${RNFS.MainBundlePath}/${(Math.random() * 1000) |
+      0}.mp3`;
 
-  downCount = () => {
-    if (this.count >= 1) {
-      this.count--;
-    } else {
-      clearInterval(timer);
+    const formUrl =
+      'http://wvoice.spriteapp.cn/voice/2015/0818/55d2248309b09.mp3'
+    const options = {
+      fromUrl: formUrl,
+      toFile: downloadDest,
+      background: true,
+      begin: res => {
+        console.log('begin', res);
+        console.log('contentLength:', res.contentLength / 1024 / 1024, 'M');
+      },
+      progress: res => {
+        let pro = res.bytesWritten / res.contentLength;
+
+        this.setState({
+          progressNum: pro,
+        });
+      }
+    };
+    try {
+      const ret = RNFS.downloadFile(options);
+      ret.promise
+        .then(res => {
+          console.log('success', res);
+
+          console.log('file://' + downloadDest);;
+
+
+            CameraRoll.saveToCameraRoll(downloadDest)
+            .then(() => {
+              Toast.showShortCenter('图片已保存到相册');;
+            })
+            .catch(() => {
+              Toast.showShortCenter('图片保存失败');;
+            });
+        })
+        .catch(err => {
+          console.log('err', err);
+        });
+    } catch (e) {
+      console.log(error);
     }
   }
-  document.addEventListener('visibilitychange', () => {
-    if (document.visibilityState == 'hidden') {
-      clearInterval(timer)
-      start = new Date().getTime()
-    } else if (document.visibilityState == 'visible') {
-      end = new Date().getTime()
-      s2 = Math.floor((end - start) / 1000)
-      this.count = this.count - s2
-      timer = setInterval(downCount, 1000)
-      document.removeEventListener('visibilitychange')
-    }
-  })
-timer = setInterval(downCount, 1000)
-  }
+}
+
